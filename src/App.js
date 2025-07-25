27bd3d764dfe468545ea2bb28088c010e0bc4a5c
Bishal Kumar Shaw <b.kumarshaw94@gmail.com>
2025-07-26 02:58:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home/Home";
-import Header from "./Components/Header/Header";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MovieDetails from "./Components/MovieDetails/MovieDetails";
 
