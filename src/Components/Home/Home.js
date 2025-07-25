26d1604030c4d788eca461ba38b54b00fb211b91
Bishal Kumar Shaw <b.kumarshaw94@gmail.com>
2025-07-26 03:08:05 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,10 +4,6 @@ import "./Home.scss";
 import Header from "../Header/Header";
 
 function Home() {
-  const array = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7,
-    8, 9,
-  ];
   return (
     <div>
       <Header />
