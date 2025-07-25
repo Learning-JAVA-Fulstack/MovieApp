ce990a0350dcbb3e039066e7dccebb2d92ced69c
Bishal Kumar Shaw <b.kumarshaw94@gmail.com>
2025-07-26 02:59:18 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MovieCards from "./MovieCards/MovieCards";
 import "./Home.scss";
 import Header from "../Header/Header";
